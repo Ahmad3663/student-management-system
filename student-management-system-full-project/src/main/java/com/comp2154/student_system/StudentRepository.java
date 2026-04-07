86da1e58d1b5b4511a9b90db2f2ee6c3edf37b36
ahmad ashoor <125370668+Ahmad3663@users.noreply.github.com>
2026-04-07 19:09:00 -0400

    Delete student-management-system-full-project directory

--- student-management-system-full-project/src/main/java/com/comp2154/student_system/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.comp2154.student_system;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
